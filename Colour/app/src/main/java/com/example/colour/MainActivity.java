2962d8532b875103e2a117b26d433719e8be5382
madhu yadav <51221544+Madhu-1729@users.noreply.github.com>
2020-01-11 12:38:07 +0530

    Update MainActivity.java

--- Colour/app/src/main/java/com/example/colour/MainActivity.java
+++ Colour/app/src/main/java/com/example/colour/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
-
+//hello
 public class MainActivity extends AppCompatActivity {
     int i;
     RelativeLayout layout;
